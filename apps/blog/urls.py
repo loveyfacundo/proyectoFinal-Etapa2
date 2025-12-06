2a67508e90dc36c54bdc665acd9698b47bbf13fe
alejandromartinez2020 <alemartinez.7722@gmail.com>
2025-12-06 00:45:47 -0300

    modularizacion y responsabilidades de estructuras html

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.index, name='index'),
+    path('nosotros/', views.about, name='about'),
+    path('contacto/', views.contact, name='contact'),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .models import Articulo, Categoria
 
-# Create your views here.
+def index(request):
+    # Traemos los artículos destacados para el carrusel
+    articulos_destacados = Articulo.objects.filter(destacado=True).order_by('-fecha_creacion')[:3]
+    
+    # Traemos las últimas noticias (excluyendo los destacados si quisieras, o todos)
+    ultimas_noticias = Articulo.objects.order_by('-fecha_creacion')[:6]
+    
+    # Traemos todas las categorías para el sidebar
+    categorias = Categoria.objects.all()
+
+    context = {
+        'destacados': articulos_destacados,
+        'noticias': ultimas_noticias,
+        'categorias': categorias,
+    }
+    return render(request, 'pages/index.html', context)
+
+# Vista para la página "Acerca de"
+def about(request):
+    return render(request, 'pages/about.html')
+
+# Vista para la página "Contacto"
+def contact(request):
+    return render(request, 'pages/contact.html')
+
