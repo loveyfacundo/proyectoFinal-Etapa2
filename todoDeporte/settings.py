8ac804d5c9ebcd5ddf73da09014d99f8839093bd
Facundo Lovey <loveyfacundoalfredo@gmail.com>
2025-12-13 20:00:08 -0300

    Agregar STATIC_ROOT para producción

--- todoDeporte/settings.py
+++ todoDeporte/settings.py
@@ -129,6 +129,9 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+# Static files (CSS, JavaScript, Images) - Para producción
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
